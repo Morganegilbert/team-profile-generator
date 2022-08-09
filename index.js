c73d2da7a8a8c14f9188b11f0aee8ecd07d2ae6a
Morgan Gilbert <morgan.el.gilbert@gmail.com>
2022-08-09 17:43:33 -0400

    updated to include questions for each employee type, added write to file function base for html

--- index.js
+++ index.js
@@ -1,82 +1,79 @@
-class Employee {
-    // Constructors
-    constructor(name, id, email) {
-        this.name = name;
-        this.id = id;
-        this.email = email;
+// Need constants to require for library
+
+// Need constant for page generation
+
+// Constants for inquirer and fs
+const inquirer = require('inquirer');
+const fs = require('fs');
+
+const questions = [
+    {
+        // Start with "Please build your team"
+        //What is the team manager's name?
+    },
+    {
+        //What is the team manager's id?
+    },
+    {
+        // What is the team manager's email?
+    },
+    {
+        // What is the team manager's office number?
+    },
+    {
+        // Next employee
+        //Which type of team member would you like to add?
+    },
+    {
+        // What is your engineer's name?
+    },
+    {
+        // What is your engineer's id?
+    },
+    {
+        // What is your engineer's email?
+    },
+    {
+        // What is your engineer's GitHub username?
+    },
+    {
+        // Next employee
+        //Which type of team member would you like to add?
+    },
+    {
+        // What is your intern's name?
+    },
+    {
+        // What is your intern's id?
+    },
+    {
+        // What is your intern's email?
+    },
+    {
+        // what is your intern's email?
     }
+]
 
-    getName(){};
 
-    getId() {};
-
-    getEmail() {};
-
-    getRole() {
-        return Employee;
-    }
+// Function to write README file - will change to create index file
+function writeToFile(fileName, readMeInfo) {
+    fs.writeFile(fileName, readMeInfo, (err) => 
+    err ? console.log(err) : console.log("Generating README...")
+    )
 }
 
-class Manager {
-    // Constructors
-    constructor(name, id, email) {
-        this.name = name;
-        this.id = id;
-        this.email = email;
-        this.officeNumber = this.officeNumber;
-    }
-
-    getName(){};
-
-    getId() {};
-
-    getEmail() {};
-
-    getRole() {
-        return Manager;
-    }
-}
-
-class Engineer {
-    // Constructors
-    constructor(name, id, email) {
-        this.name = name;
-        this.id = id;
-        this.email = email;
-        this.github = this.github;
-    }
-
-    getName(){};
-
-    getId() {};
-
-    getEmail() {};
-
-    getGithub() {};
-
-    getRole() {
-        return Engineer;
-    }
-}
-
-class Intern {
-    // Constructors
-    constructor(name, id, email) {
-        this.name = name;
-        this.id = id;
-        this.email = email;
-        this.school = this.school;
-    }
-
-    getName(){};
-
-    getId() {};
-
-    getEmail() {};
-
-    getSchool() {};
+// Function to initialize app and activate markdown function
+function init() {
+    inquirer.prompt(questions)
+    .then((answers) => {
+        let fileName = "index.html";
+        let license = answers.license;
+        let readMeInfo = generateMarkdown(answers, license);
+        //Use user feedback for...
+        writeToFile(fileName, readMeInfo);
     
-    getRole() {
-        return Intern;
-    }
+    })
 }
+
+// Function call to initialize app
+init();
