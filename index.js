6e39197d240f71bebbaab334fecec1546aa61e5c
Morgan Gilbert <morgan.el.gilbert@gmail.com>
2022-08-10 20:05:03 -0400

    updated prompts in index along with .then statement started to start next employee prompt. Adjusted html name and generateMarkdown in index. Added constants for employee types and markdown links. Added module.export to employee to pass tests.

--- index.js
+++ index.js
@@ -1,15 +1,22 @@
 // Need constants to require for library
+const Manager = require("./lib/Manager");
+const Engineer = require("./lib/Engineer");
+const Intern = require("./lib/Intern");
 
 // Need constant for page generation
-
+const generateMarkdown = require("./src/generateHTML")
 // Constants for inquirer and fs
 const inquirer = require('inquirer');
 const fs = require('fs');
 
+// Const for Employees
+const employeeGroup = [];
+
 // Constant for manager questions
 const questionsManager = [
-    console.log('Please build your team.')
+    // console.log('Please build your team.'),
     //Questions for Manager
+
     {
         //What is the team manager's name?
         type: "input",
@@ -55,7 +62,7 @@ const questionsManager = [
     {
         // What is the team manager's office number?
         type: "input",
-        name: "phone",
+        name: "officeNumber",
         message: "What is the team manager's office number?",
         // validate: usernameInput => {
         //     if (usernameInput) {
@@ -68,10 +75,7 @@ const questionsManager = [
     },
 ]
 
-// Constant for engineer questions
-const questionsEngineer = [
-
-    // Questions for Engineer
+const chooseRole = [
     {
         // Next employee
         //Which type of team member would you like to add?
@@ -88,158 +92,174 @@ const questionsEngineer = [
         //     }
         //   }
     },
-    {
-        // What is your engineer's name?
-        type: "input",
-        name: "name",
-        message: "What is your engineer's name?",
-        // validate: usernameInput => {
-        //     if (usernameInput) {
-        //       return true;
-        //     } else {
-        //       console.log('You need to enter a GitHub username!');
-        //       return false;
-        //     }
-        //   }
-    },
-    {
-        // What is your engineer's id?
-        type: "input",
-        name: "id",
-        message: "What is your engineer's id?",
-        // validate: usernameInput => {
-        //     if (usernameInput) {
-        //       return true;
-        //     } else {
-        //       console.log('You need to enter a GitHub username!');
-        //       return false;
-        //     }
-        //   }
-    },
-    {
-        // What is your engineer's email?
-        type: "input",
-        name: "email",
-        message: "What is your engineer's email?",
-        // validate: usernameInput => {
-        //     if (usernameInput) {
-        //       return true;
-        //     } else {
-        //       console.log('You need to enter a GitHub username!');
-        //       return false;
-        //     }
-        //   }
-    },
-    {
-        // What is your engineer's GitHub username?
-        type: "input",
-        name: "github",
-        message: "What is your engineer's GitHub username?",
-        // validate: usernameInput => {
-        //     if (usernameInput) {
-        //       return true;
-        //     } else {
-        //       console.log('You need to enter a GitHub username!');
-        //       return false;
-        //     }
-        //   }
-    },
 ]
+// Constant for engineer questions
+// const questionsEngineer = [
 
-// Constant for intern questions
-const questionsIntern = [
+//     // Questions for Engineer
 
-    // Questions for Intern
-    {
-        // Next employee
-        //Which type of team member would you like to add?
-        type: "input",
-        name: "phone",
-        message: "Which type of team member would you like to add?",
-        // validate: usernameInput => {
-        //     if (usernameInput) {
-        //       return true;
-        //     } else {
-        //       console.log('You need to enter a GitHub username!');
-        //       return false;
-        //     }
-        //   }
-    },
-    {
-        // What is your intern's name?
-        type: "input",
-        name: "name",
-        message: "What is your intern's name?",
-        // validate: usernameInput => {
-        //     if (usernameInput) {
-        //       return true;
-        //     } else {
-        //       console.log('You need to enter a GitHub username!');
-        //       return false;
-        //     }
-        //   }
-    },
-    {
-        // What is your intern's id?
-        type: "input",
-        name: "id",
-        message: "What is your intern's id?",
-        // validate: usernameInput => {
-        //     if (usernameInput) {
-        //       return true;
-        //     } else {
-        //       console.log('You need to enter a GitHub username!');
-        //       return false;
-        //     }
-        //   }
-    },
-    {
-        // What is your intern's email?
-        type: "input",
-        name: "email",
-        message: "What is your intern's email?",
-        // validate: usernameInput => {
-        //     if (usernameInput) {
-        //       return true;
-        //     } else {
-        //       console.log('You need to enter a GitHub username!');
-        //       return false;
-        //     }
-        //   }
-    },
-    {
-        // What is your intern's school?
-        type: "input",
-        name: "school",
-        message: "What is your intern's school?",
-        // validate: usernameInput => {
-        //     if (usernameInput) {
-        //       return true;
-        //     } else {
-        //       console.log('You need to enter a GitHub username!');
-        //       return false;
-        //     }
-        //   }
-    }
-]
+//     {
+//         // What is your engineer's name?
+//         type: "input",
+//         name: "name",
+//         message: "What is your engineer's name?",
+//         // validate: usernameInput => {
+//         //     if (usernameInput) {
+//         //       return true;
+//         //     } else {
+//         //       console.log('You need to enter a GitHub username!');
+//         //       return false;
+//         //     }
+//         //   }
+//     },
+//     {
+//         // What is your engineer's id?
+//         type: "input",
+//         name: "id",
+//         message: "What is your engineer's id?",
+//         // validate: usernameInput => {
+//         //     if (usernameInput) {
+//         //       return true;
+//         //     } else {
+//         //       console.log('You need to enter a GitHub username!');
+//         //       return false;
+//         //     }
+//         //   }
+//     },
+//     {
+//         // What is your engineer's email?
+//         type: "input",
+//         name: "email",
+//         message: "What is your engineer's email?",
+//         // validate: usernameInput => {
+//         //     if (usernameInput) {
+//         //       return true;
+//         //     } else {
+//         //       console.log('You need to enter a GitHub username!');
+//         //       return false;
+//         //     }
+//         //   }
+//     },
+//     {
+//         // What is your engineer's GitHub username?
+//         type: "input",
+//         name: "github",
+//         message: "What is your engineer's GitHub username?",
+//         // validate: usernameInput => {
+//         //     if (usernameInput) {
+//         //       return true;
+//         //     } else {
+//         //       console.log('You need to enter a GitHub username!');
+//         //       return false;
+//         //     }
+//         //   }
+//     },
+// ]
+
+// // Constant for intern questions
+// const questionsIntern = [
+
+//     // Questions for Intern
+//     {
+//         // Next employee
+//         //Which type of team member would you like to add?
+//         type: "input",
+//         name: "officeNumber",
+//         message: "Which type of team member would you like to add?",
+//         // validate: usernameInput => {
+//         //     if (usernameInput) {
+//         //       return true;
+//         //     } else {
+//         //       console.log('You need to enter a GitHub username!');
+//         //       return false;
+//         //     }
+//         //   }
+//     },
+//     {
+//         // What is your intern's name?
+//         type: "input",
+//         name: "name",
+//         message: "What is your intern's name?",
+//         // validate: usernameInput => {
+//         //     if (usernameInput) {
+//         //       return true;
+//         //     } else {
+//         //       console.log('You need to enter a GitHub username!');
+//         //       return false;
+//         //     }
+//         //   }
+//     },
+//     {
+//         // What is your intern's id?
+//         type: "input",
+//         name: "id",
+//         message: "What is your intern's id?",
+//         // validate: usernameInput => {
+//         //     if (usernameInput) {
+//         //       return true;
+//         //     } else {
+//         //       console.log('You need to enter a GitHub username!');
+//         //       return false;
+//         //     }
+//         //   }
+//     },
+//     {
+//         // What is your intern's email?
+//         type: "input",
+//         name: "email",
+//         message: "What is your intern's email?",
+//         // validate: usernameInput => {
+//         //     if (usernameInput) {
+//         //       return true;
+//         //     } else {
+//         //       console.log('You need to enter a GitHub username!');
+//         //       return false;
+//         //     }
+//         //   }
+//     },
+//     {
+//         // What is your intern's school?
+//         type: "input",
+//         name: "school",
+//         message: "What is your intern's school?",
+//         // validate: usernameInput => {
+//         //     if (usernameInput) {
+//         //       return true;
+//         //     } else {
+//         //       console.log('You need to enter a GitHub username!');
+//         //       return false;
+//         //     }
+//         //   }
+//     }
+// ]
 
 
 // Function to write README file - will change to create index file
-function writeToFile(fileName, readMeInfo) {
-    fs.writeFile(fileName, readMeInfo, (err) => 
-    err ? console.log(err) : console.log("Generating README...")
+function writeToFile(fileName, htmlInfo) {
+    fs.writeFile(fileName, htmlInfo, (err) => 
+    err ? console.log(err) : console.log("Generating Team...")
     )
 }
 
 // Function to initialize app and activate markdown function
 function init() {
-    inquirer.prompt(questions)
+    console.log("Please build your team.");
+    inquirer.prompt(questionsManager)
+    // pushes manager info
+    .then((managerInfo) => {
+        const {name, id, email, officeNumber} = managerInfo;
+    
+        employeeGroup.push(Manager);
+        console.log(Manager)
+    })
+    // then moves to next employee
+    // .then(function({chooseRole}))
     .then((answers) => {
-        let fileName = "index.html";
-        let license = answers.license;
-        let readMeInfo = generateMarkdown(answers, license);
+        let fileName = "./dist/index.html";
+        // let license = answers.license;
+        let htmlInfo = generateMarkdown(employeeGroup);
         //Use user feedback for...
-        writeToFile(fileName, readMeInfo);
+        writeToFile(fileName, htmlInfo);
     
     })
 }
